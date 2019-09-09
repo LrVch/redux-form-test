075034c7fd4ad89bd4969b48b35636d227d94831
RLonskiy <RLonskiy@luxoft.com>
2019-09-09 07:59:44 +0300

    simple

--- src/components/SimpleForm/SimpleForm.js
+++ src/components/SimpleForm/SimpleForm.js
@@ -1,47 +1,128 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
-const SimpleForm = (props) => {
-  const { handleSubmit } = props
+const SimpleForm = ({ handleSubmit, pristine, reset, submitting }) => {
   return <form
     className="mt-3"
     noValidate
     onSubmit={handleSubmit}>
-    <div>
+
+    <div className="form-group">
       <label htmlFor="firstName">First Name</label>
-      <div className="input-group">
-        <Field
-          placeholder="First Name"
-          className="form-control"
-          name="firstName"
-          component="input"
-          type="text"
-        />
-      </div>
+      <Field
+        id="firstName"
+        placeholder="First Name"
+        className="form-control"
+        name="firstName"
+        component="input"
+        type="text"
+      />
     </div>
-    <div className="mt-3">
+
+    <div className="form-group mt-3">
       <label htmlFor="lastName">Last Name</label>
-      <div className="input-group">
-        <Field
-          placeholder="Last Name"
-          className="form-control"
-          name="lastName"
-          component="input"
-          type="text" />
-      </div>
+      <Field
+        id="lastName"
+        placeholder="Last Name"
+        className="form-control"
+        name="lastName"
+        component="input"
+        type="text" />
     </div>
-    <div className="mt-3">
+
+    <div className="form-group mt-3">
       <label htmlFor="email">Email</label>
-      <div className="input-group">
+      <Field
+        id="email"
+        placeholder="Email"
+        className="form-control"
+        name="email"
+        component="input"
+        type="email" />
+    </div>
+
+    <div >
+      <label>Sex</label>
+      <div className="form-group form-check">
         <Field
-          placeholder="Email"
-          className="form-control"
-          name="email"
+          id="male"
+          className="form-check-input"
+          name="sex"
           component="input"
-          type="email" />
+          type="radio"
+          value="male" />
+        <label
+          className="form-check-label"
+          htmlFor="male">Male</label>
+      </div>
+      <div className="form-group form-check">
+        <Field
+          id="female"
+          className="form-check-input"
+          name="sex"
+          component="input"
+          type="radio"
+          value="female" />
+        <label
+          className="form-check-label"
+          htmlFor="female">Female</label>
+      </div>
+      <div className="form-group form-check">
+        <Field
+          id="other"
+          className="form-check-input"
+          name="sex"
+          component="input"
+          type="radio"
+          value="other" />
+        <label
+          className="form-check-label"
+          htmlFor="other">Other</label>
       </div>
     </div>
-    <button type="submit" className="btn btn-secondary mt-3">Submit</button>
+
+    <div className="form-group mt-3">
+      <label htmlFor="favoriteColor">Favorite Color</label>
+      <div>
+        <Field
+          className="form-control"
+          id="favoriteColor"
+          name="favoriteColor"
+          component="select">
+          <option />
+          <option value="ff0000">Red</option>
+          <option value="00ff00">Green</option>
+          <option value="0000ff">Blue</option>
+        </Field>
+      </div>
+    </div>
+
+    <div className="form-group form-check">
+      <Field
+        id="employed"
+        className="form-check-input"
+        name="employed"
+        component="input"
+        type="checkbox" />
+      <label
+        className="form-check-label"
+        htmlFor="employed">Employed</label>
+    </div>
+
+    <div className="form-group">
+      <label htmlFor="notes">Notes</label>
+      <div>
+        <Field
+          className="form-control"
+          id="notes"
+          name="notes"
+          component="textarea" />
+      </div>
+    </div>
+
+    <button
+      type="submit"
+      className="btn btn-secondary mt-3">Submit</button>
   </form>
 }
 
