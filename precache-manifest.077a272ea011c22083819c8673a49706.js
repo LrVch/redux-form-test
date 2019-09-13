ba80389419602fdcc5095af50109ab5cb6983c6b
Roman Lonskiy <rlonskiy@luxoft.com>
2019-09-13 10:05:58 +0300

    Updates

---
+++ precache-manifest.077a272ea011c22083819c8673a49706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe06d4ed985604617920fd2ab251b91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6830f2bb8ad0b496feba",
+    "url": "/static/css/main.2d6b49da.chunk.css"
+  },
+  {
+    "revision": "dae453798197094cf5f4",
+    "url": "/static/js/2.dcf6ef71.chunk.js"
+  },
+  {
+    "revision": "6830f2bb8ad0b496feba",
+    "url": "/static/js/main.4769d782.chunk.js"
+  },
+  {
+    "revision": "ede3a563c55f26d4fa86",
+    "url": "/static/js/runtime~main.00229bde.js"
+  }
+]);
